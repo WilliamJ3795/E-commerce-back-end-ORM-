1830c13a9880902927980f1f505c17e44002337a
William James <Williamj3795@gmail.com>
2022-11-19 13:10:29 -0500

    Tag. belongsToMany Product

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -19,7 +19,12 @@ Product.belongsToMany(Tag, {
   through: ProductTag,
   foreignKey: "product_id",
 });
+
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: "tag_id",
+});
 
 module.exports = {
   Product,
